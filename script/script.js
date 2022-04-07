758d0a4a6c3925b7c9095860097632dac91fabf1
02Ian <mohitathani15598@gmail.com>
2022-04-08 00:35:25 +0530

    First JS code for compare 3 numbers.

---
+++ script/script.js
@@ -0,0 +1,29 @@
+// Declare three variables num1, num2 and num3 & initialize them with number values
+var a = 10;
+var b = 12;
+var c = 14;
+// Write code to check which is the smallest number amongst num1, num2 and num3
+
+if(a>b){
+    if(a>c){
+        console.log(a);
+    }
+    else{
+        console.log(c);
+    }
+    }
+    else{
+        if(b>c){
+            console.log(b);
+        }
+    else{
+        console.log(c);
+    }
+}
+var valaue1 = 0;
+var value = '0';
+if(valaue1 == value)
+{
+    console.log("Matched");
+}
+console.log(typeof('5'), typeof(' '));
