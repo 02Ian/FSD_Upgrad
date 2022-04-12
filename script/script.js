7f62a830a91f81127702b73bed7298d7e564ef1e
02Ian <mohitathani15598@gmail.com>
2022-04-12 11:24:31 +0530

    Update things.

--- script/script.js
+++ script/script.js
@@ -1,29 +1,57 @@
-// Declare three variables num1, num2 and num3 & initialize them with number values
-var a = 10;
-var b = 12;
-var c = 14;
-// Write code to check which is the smallest number amongst num1, num2 and num3
+// // Declare three variables num1, num2 and num3 & initialize them with number values
+// var a = 10;
+// var b = 12;
+// var c = 14;
+// // Write code to check which is the smallest number amongst num1, num2 and num3
+// if(a>b){
+//     if(a>c){
+//         console.log(a);
+//     }
+//     else{
+//         console.log(c);
+//     }
+//     }
+//     else{
+//         if(b>c){
+//             console.log(b);
+//         }
+//     else{
+//         console.log(c);
+//     }
+// }
 
-if(a>b){
-    if(a>c){
-        console.log(a);
-    }
-    else{
-        console.log(c);
-    }
-    }
-    else{
-        if(b>c){
-            console.log(b);
-        }
-    else{
-        console.log(c);
-    }
+
+// var number = 4567, sum = 0;
+// while(number){
+//     sum += number % 10;
+//     number = Math.floor(number / 10);
+// }
+// console.log(sum);
+// var match = {
+//     team1: "Manchester City",
+//     team2: "Liverpool",
+//     winner: function() {
+//         alert("Manchester City beats Liverpool by 5-0");
+//     }
+// }
+
+// match.winner();
+var n = 2;
+for (var i = n; i < 12; i = i + n) {
+  console.log(i);
 }
-var valaue1 = 0;
-var value = '0';
-if(valaue1 == value)
-{
-    console.log("Matched");
-}
-console.log(typeof('5'), typeof(' '));
+
+// function f() {
+//     var x = function(){
+//         return 4*4;
+//     };
+//     alert("Alter data: "+x);
+// };
+
+// var valaue1 = 0;
+// var value = '0';
+// if(valaue1 == value)
+// {
+//     console.log("Matched");
+// }
+// console.log(typeof('5'), typeof(' '));
